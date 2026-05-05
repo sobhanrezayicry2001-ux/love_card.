a14cee96797e75524c9dc47ff3f1af8556ebb1ac
sobhanrezayicry2001-ux <sobhanrezayicry2001@gmail.com>
2026-05-05 17:24:11 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const text = ملیکا...
-
-هنر یعنی
-یه فضا رو انقدر قشنگ بسازی
-که آدم
-دلش بخواد
-توش زندانی بشه...
-
-من سبحانم،
-زندانی شماره 0668
-
-و یه درخواست دارم—
-
-یه ملاقات کوتاه
-با مدیر این فضا
-برای یه روز نزدیک...;
-
-let i = 0;
-
-function typeWriter() {
-  if (i < text.length) {
-    document.getElementById("text").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, 50);
-  }
-}
+console.log("JS LOADED");
 
 function startExperience() {
+  // مخفی کردن صفحه شروع
   document.getElementById("startScreen").style.display = "none";
+
+  // نمایش محتوای اصلی
   document.getElementById("mainContent").classList.remove("hidden");
 
+  // پخش موزیک
   const music = document.getElementById("music");
-  music.play();
+  music.play().catch(err => {
+    console.log("Autoplay blocked:", err);
+  });
+
+  // متن تایپی
+  const text = ملیکا 
+هنر یعنی یه فضا رو اینقد قشنگ بسازی ک آدم دلش بخواد توش زندانی بشه 
+من سبحانم 
+زندانی شماره ۰۶۶۸
+یه درخواست دارم 
+یه ملاقات خودمونی با خانم مدیر برای یه روز نزدیک...;
+
+  let i = 0;
+  const speed = 50;
+
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("text").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
 
   typeWriter();
 }
